34fc640f88b3c50bc2a873e3022e9e10d430091f
Rasuli Azamat <rasuliazamat@gmail.com>
2022-06-01 12:38:53 +0500

    maded error page adaptive

--- pages/[...404].vue
+++ pages/[...404].vue
@@ -1,11 +1,11 @@
 <template>
-  <section class="max-w-appContainer mx-auto">
-    <div class="w-1/2 mx-auto mb-16 text-center">
-      <h1 class="mb-9 text-5xl text-danger">404 ERROR</h1>
-      <p class="mb-14 text-3xl text-slate-500">
+  <section class="max-w-appContainer mx-auto px-4">
+    <div class="w-full md:w-1/2 mx-auto mb-16 text-center">
+      <h1 class="mb-9 text-3xl md:text-5xl text-danger">404 ERROR</h1>
+      <p class="mb-14 text-lg md:text-3xl text-slate-500">
         Такой страницы не существует. Вернитесь пожалуйста на главную чтобы продолжить поиск
       </p>
-      <ElementButton class="py-2 px-10">
+      <ElementButton class="py-2 px-5 md:px-10">
         <NuxtLink to="/">Главная страница</NuxtLink>
       </ElementButton>
     </div>
